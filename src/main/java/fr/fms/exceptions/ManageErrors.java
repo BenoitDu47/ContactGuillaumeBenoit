6f9bf65aa129821df5950f9826f53b906e3f5018
Benoit THOURY <benoit.thoury@gmail.com>
2023-06-27 10:20:27 +0200

    start -> controller and business

---
+++ src/main/java/fr/fms/exceptions/ManageErrors.java
@@ -0,0 +1,13 @@
+package fr.fms.exceptions;
+
+/**
+ * Classe utilitaire pour la gestion des erreurs.
+ * Cette classe fournit des constantes et des méthodes pour la gestion des erreurs dans l'application.
+ */
+public class ManageErrors {
+	/**
+	 * Constructeur privé pour empêcher l'instanciation de la classe.
+	 */
+	private ManageErrors() {}
+	public static final String STR_ERROR = "mauvaise saisie";
+}
